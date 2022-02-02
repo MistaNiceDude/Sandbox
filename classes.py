b7ebe0de6c8a2fd3cc5a6e69d12b14db149cc248
Rodney Thompson <rodneythompson@rodneys-mbp.home>
2022-02-02 01:53:22 -0500

    tried more collisions with no success and added player positions to screen.

--- classes.py
+++ classes.py
@@ -7,15 +7,26 @@ green = (0, 255,0)
 
 WIN = WIDTH, HEIGHT = 800, 800
 surface = pygame.display.set_mode(WIN)
+LEFT_COLLIDE = 0
+RIGHT_COLLIDE = 1
+TOP_COLLIDE = 2
+BOTTOM_COLLIDE = 3
+def collide(obj1, obj2):
+    offset_x = (obj2.x + 2) - obj1.x
+    offset_y = (obj2.y + 2) - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 
-class Player:
 
+
+class Player():
     def __init__(self, x, y):
         self.player = pygame.Surface((40,40))
         self.x = x
         self.y = y
-        self.rect = pygame.Rect(self.x, self.y, 40, 40)
+        self.rect = pygame.Rect(self.x, self.y, self.get_width(), self.get_height())
+        self.mask = pygame.mask.from_surface(self.player)
+        #self.collide = collide(Player, obj2)
 
     def draw(self, WINDOW: pygame.Surface):
         WINDOW.blit(self.player, (self.x, self.y))
@@ -32,6 +43,28 @@ class Player:
         self.y = self.y + y
         self.rect.x = self.x
         self.rect.y = self.y
+
+    def get_rect(self):
+        return self.player.get_rect()
+        
+
+
+    def collision(self, obj):
+        return collide(self, obj)
+
+    #def collide_dir(self, rect: pygame.Rect):
+    #        block_rect = rect
+    #        if block_rect.collidepoint(self.rect.midleft):
+    #            return LEFT_COLLIDE
+    #        elif block_rect.collidepoint(self.rect.midbottom):
+    #            return BOTTOM_COLLIDE
+    #        elif block_rect.collidepoint(self.rect.midtop):
+    #            return TOP_COLLIDE
+    #        elif block_rect.collidepoint(self.rect.midright):
+    #            return RIGHT_COLLIDE
+    #        else:
+    #            return 4
+
 
 
     def move(self, DISPLAY):
@@ -50,13 +83,16 @@ class Player:
         if keys[pygame.K_d] and self.x + self.player.get_width() < WIDTH:
             self.transform(player_vel, 0)
 
-class Block:
+
+class Block():
+
 
     def __init__(self, x, y):
         self.block = pygame.Surface((40,40))
         self.x = x
         self.y = y
-        self.rect = pygame.Rect(self.x, self.y, 40, 40)
+        self.rect = pygame.Rect(self.x, self.y, self.get_width(), self.get_height())
+        self.mask = pygame.mask.from_surface(self.block)
 
     def draw(self, WINDOW: pygame.Surface):
         WINDOW.blit(self.block, (self.x, self.y))
@@ -73,6 +109,24 @@ class Block:
         self.y = self.y + y
         self.rect.x = self.x
         self.rect.y = self.y
+        vel = 5
 
+    def get_rect(self):
+        return self.block.get_rect()
 
+    def collision(self, obj):
+            return collide(self, obj)
+
+    #def collide_dir(self, rect: pygame.Rect):
+    #        player_rect = rect
+    #        if player_rect.collidepoint(self.rect.midleft):
+    #            return LEFT_COLLIDE
+    #        elif player_rect.collidepoint(self.rect.midbottom):
+    #            return BOTTOM_COLLIDE
+    #        elif player_rect.collidepoint(self.rect.midtop):
+    #            return TOP_COLLIDE
+    #        elif player_rect.collidepoint(self.rect.midright):
+    #            return RIGHT_COLLIDE
+    #        else:
+    #            return 4
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import time
 from classes import *
 
+pygame.font.init()
 pygame.init()
 pygame.display.init()
 
@@ -18,14 +20,40 @@ player = Player(WIDTH/2, HEIGHT/2)
 player.x = 400
 player.y = 400
 block = Block(100, 100)
+clock = pygame.time.Clock()
+FPS = 60
+clock_font = pygame.font.SysFont("comicsans", 20)
+player.x_font = pygame.font.SysFont("comicsans", 20)
+player.y_font = pygame.font.SysFont("comicsans", 20)
 
-
-while run:
+def redraw():
+    player.x_m = (f"Player.x: {player.x}")
+    player.y_m = (f"Player.y: {player.y}")
+    DISPLAY.fill(black)
     DISPLAY.fill(black)
     player.draw(DISPLAY)
     player.move(DISPLAY)
     block.draw(DISPLAY)
+    clock_label = clock_font.render(f"FPS: {clock}", 2, (255, 255, 255))
+    player.x_label = player.x_font.render(f" {player.x_m}", 2, (255, 255, 255))
+    player.y_label = player.y_font.render(f" {player.y_m}", 2, (255, 255, 255))
+    DISPLAY.blit(clock_label, (20, HEIGHT - clock_label.get_height() - 20))
+    DISPLAY.blit(player.x_label, (WIDTH - player.x_label.get_width() - 20, HEIGHT - player.x_label.get_height() - 40))
+    DISPLAY.blit(player.y_label, (WIDTH - player.y_label.get_width() - 20, HEIGHT - player.y_label.get_height() - 20))
 
+
+while run:
+    clock.tick(FPS)
+    redraw()
+
+
+    collision = collide(player, block)
+    if collision == True:
+        if player.x > block.x + block.get_width()/2 and block.x > 0:
+            block.move(0, -Player.player_vel)
+
+
+    #print(player.collide_dir(Player))
 
 
     # pygame.display.update()
